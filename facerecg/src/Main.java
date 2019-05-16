dd1308af339d8d8ba677f2caf3e503da299275b6
hujianbest <jianbesthu@163.com>
2019-05-16 15:17:16 +0800

    update

--- facerecg/src/Main.java
+++ facerecg/src/Main.java
@@ -20,69 +20,67 @@ public class Main extends JPanel {
 
     private BufferedImage mImg;
 
-    private BufferedImage mat2BI(Mat mat){
-        int dataSize =mat.cols()*mat.rows()*(int)mat.elemSize();
-        byte[] data=new byte[dataSize];
-        mat.get(0, 0,data);
-        int type=mat.channels()==1?
-                BufferedImage.TYPE_BYTE_GRAY:BufferedImage.TYPE_3BYTE_BGR;
 
-        if(type==BufferedImage.TYPE_3BYTE_BGR){
-            for(int i=0;i<dataSize;i+=3){
-                byte blue=data[i+0];
-                data[i+0]=data[i+2];
-                data[i+2]=blue;
+    // mat to buffered image
+    private BufferedImage mat2BI(Mat mat) {
+        int dataSize = mat.cols() * mat.rows() * (int) mat.elemSize();
+        byte[] data = new byte[dataSize];
+        mat.get(0, 0, data);
+        int type = mat.channels() == 1 ?
+                BufferedImage.TYPE_BYTE_GRAY : BufferedImage.TYPE_3BYTE_BGR;
+
+        if (type == BufferedImage.TYPE_3BYTE_BGR) {
+            for (int i = 0; i < dataSize; i += 3) {
+                byte blue = data[i + 0];
+                data[i + 0] = data[i + 2];
+                data[i + 2] = blue;
             }
         }
-        BufferedImage image=new BufferedImage(mat.cols(),mat.rows(),type);
+        BufferedImage image = new BufferedImage(mat.cols(), mat.rows(), type);
         image.getRaster().setDataElements(0, 0, mat.cols(), mat.rows(), data);
 
         return image;
     }
 
-    public void paintComponent(Graphics g){
-        if(mImg!=null){
-            g.drawImage(mImg, 0, 0, mImg.getWidth(),mImg.getHeight(),this);
+    public void paintComponent(Graphics g) {
+        if (mImg != null) {
+            g.drawImage(mImg, 0, 0, mImg.getWidth(), mImg.getHeight(), this);
         }
     }
 
     public static void main(String[] args) {
-        try{
+        try {
             System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-            Mat capImg=new Mat();
-            VideoCapture capture=new VideoCapture(0);
-            int height = (int)capture.get(Videoio.CAP_PROP_FRAME_HEIGHT);
-            int width = (int)capture.get(Videoio.CAP_PROP_FRAME_WIDTH);
-            if(height==0||width==0){
-                throw new Exception("camera not found!");
+            Mat capImg = new Mat();//opencv 图像处理封装类
+            VideoCapture capture = new VideoCapture(0);//视频捕捉
+            int height = (int) capture.get(Videoio.CAP_PROP_FRAME_HEIGHT);
+            int width = (int) capture.get(Videoio.CAP_PROP_FRAME_WIDTH);
+            if (height == 0 || width == 0) {
+                throw new Exception("Camera not found!");
             }
 
-            JFrame frame=new JFrame("camera");
+            JFrame frame = new JFrame("Camera");
             frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            Main panel=new Main();
+            Main panel = new Main();
 
             frame.setContentPane(panel);
             frame.setVisible(true);
-            frame.setSize(width+frame.getInsets().left+frame.getInsets().right,
-                    height+frame.getInsets().top+frame.getInsets().bottom);
-            int n=0;
-            Mat temp=new Mat();
-            while(frame.isShowing()&& n<500){
-                //System.out.println("第"+n+"张");
-                capture.read(capImg);
-                Imgproc.cvtColor(capImg, temp, Imgproc.COLOR_RGB2GRAY);
+            frame.setSize(width + frame.getInsets().left + frame.getInsets().right,
+                    height + frame.getInsets().top + frame.getInsets().bottom);
+            Mat grayImg = new Mat();
+            while (frame.isShowing() ) {
+                capture.read(capImg);//输出到mat
+                Imgproc.cvtColor(capImg, grayImg, Imgproc.COLOR_RGB2GRAY);//彩色空间转换，grayImg为目标mat
                 //Imgcodecs.imwrite("G:/opencv/lw/neg/back"+n+".png", temp);
-                panel.mImg=panel.mat2BI(detectFace(capImg));
+                panel.mImg = panel.mat2BI(detectFace(capImg));
                 panel.repaint();
-                //n++;
-                //break;
             }
             capture.release();
             frame.dispose();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("异常：" + e);
-        }finally{
+        } finally {
             System.out.println("--done--");
         }
 
@@ -90,28 +88,26 @@ public class Main extends JPanel {
 
     /**
      * opencv实现人脸识别
+     *
      * @param img
      */
-    public static Mat detectFace(Mat img) throws Exception
-    {
+    public static Mat detectFace(Mat img) throws Exception {
 
-        System.out.println("Running DetectFace ... ");
+//        System.out.println("Running DetectFace ... ");
         // 从配置文件lbpcascade_frontalface.xml中创建一个人脸识别器，该文件位于opencv安装目录中
         CascadeClassifier faceDetector = new CascadeClassifier("D:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml");
 
 
         // 在图片中检测人脸
         MatOfRect faceDetections = new MatOfRect();
-
         faceDetector.detectMultiScale(img, faceDetections);
 
         //System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
 
         Rect[] rects = faceDetections.toArray();
-        if(rects != null && rects.length >= 1){
+        if (rects != null && rects.length >= 1) {
             for (Rect rect : rects) {
-                Imgproc.rectangle(img, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),new Scalar(0, 0, 255), 2);
-//                Imgproc.rectangle(img,new Poin);
+                Imgproc.rectangle(img, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 2);
             }
         }
         return img;
