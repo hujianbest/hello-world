74fceb1e8ddfc89786842271b80723bd7c33ea89
hujianbest <jianbesthu@163.com>
2019-07-24 23:30:07 +0800

    update

---
+++ gettounderstandspring/src/main/java/org/hujianbest/ioc/bean/MyTestBean.java
@@ -0,0 +1,13 @@
+package org.hujianbest.ioc.bean;
+
+public class MyTestBean {
+    private String  testStr  = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
