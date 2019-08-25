ae8e243c9173ba6ed0b1d7d9de993093646e334d
hujianbest <jianbesthu@163.com>
2019-08-25 11:57:08 +0800

    spring ioc的使用

--- gettounderstandspring/src/main/java/org/hujianbest/ioc/bean/Huge.java
+++ gettounderstandspring/src/main/java/org/hujianbest/ioc/bean/Huge.java
@@ -3,8 +3,18 @@ package org.hujianbest.ioc.bean;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class Huge {
+
+    public  Huge(){
+        System.out.println("调用了无参构造器");
+    }
+    @PostConstruct
+    public void Huge(){
+        System.out.println("调用了@PostConstruct");
+    }
     public String say(){
         return "I'm huge";
     }
--- gettounderstandspring/src/main/java/org/hujianbest/ioc/bean/Main.java
+++ gettounderstandspring/src/main/java/org/hujianbest/ioc/bean/Main.java
@@ -7,15 +7,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
  * 依赖注入使用在被Spring管理的类中，简单说也就是加了@Controller @Service @Repository @Component注解的类中
  * 在没有被Spring管理的类中获取bean需使用ApplicationContext.getBean()
+ *
+ * 底层的类交给Spring管理的时候调用的什么构造器，无参构造器、@PostConstruct、或者指定init方法
  */
 public class Main {
-
-    private  static AutoBean autoBean;
 
     public static void main(String[] args) {
 //        ApplicationContext context = new ClassPathXmlApplicationContext("beanFactoryTest.xml");
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
-        autoBean = context.getBean("autoBean", AutoBean.class);
+        AutoBean autoBean = context.getBean("autoBean", AutoBean.class);
         System.out.println(autoBean.say());
     }
 }
